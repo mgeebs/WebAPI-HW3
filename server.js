a3abe1fd4ad7b9aba370286d5dd350bde4faf586
michael.hedrick@ucdenver.edu <michael.hedrick@ucdenver.edu>
2018-04-20 21:06:42 -0600

    another dumb commit

--- server.js
+++ server.js
@@ -229,11 +229,8 @@ router.route('/movies')
         Movie.find(function (err, movies) {
 
                 if (err) res.status(404).send(err);
-            if (reviewsQuery != "true"){
-                res.json(movie);
-            }
-
-            else res.json(movies);
+                // return the movies
+                else res.json(movies);
 
         });
     });
